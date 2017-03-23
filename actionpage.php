54f416f235e9d9c2039af2d764e0380670d03e7d
Souww101 <souuw101zidm@gmail.com>
2017-03-23 14:05:17 +0000

    Update actionpage.php

--- actionpage.php
+++ actionpage.php
@@ -1,1 +1,18 @@
+    <?php
+    $recipient = "souuw101zidm@gmail.com";
+    $subject   = "This is a test e-mail";
+    $message   = "Hi!
 
+    This is a test message (e-mail body).
+    This is a new line
+
+    Enough for now.
+    Best regards,
+
+    Test test
+    ";
+
+    mail($recipient, $subject, $message);
+
+    header('Location:Lore.html');
+    ?>
